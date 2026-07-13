099ecb8a19be61baaa87cbdae98de19b7c535c21
Shubham Raj <sprashar889@gmail.com>
2026-07-13 07:58:04 +0530

    Encapsulation Completed

---
+++ OOPS CONCEPT/testEncapsulation.java
@@ -0,0 +1,17 @@
+public class testEncapsulation {
+    public static void main(String[] args) {
+        encapsulation A = new encapsulation(1, 12, "Rahul", 8, "Tina");
+
+        System.out.println(A.name);
+        System.out.println(A.age);
+        System.out.println(A.id);
+        System.out.println(A.nos);
+        System.out.println(A.getGfName());
+         A.setGf("Soni");
+        System.out.println(A.getGfName());
+
+        A.bunk();
+        A.sleep();
+        A.startChatting();
+    }
+}
