c07965706cbe58734773e72d125059e4ecfbb508
Shubham Raj <sprashar889@gmail.com>
2026-07-21 16:36:06 +0530

    Finding Unique Element

---
+++ unique_element/uniqueElement.java
@@ -0,0 +1,16 @@
+package unique_element;
+
+public class uniqueElement {
+    static int findUniqueElement(int[] nums) {
+        int xorSum = 0;
+        for (int n : nums) {
+            xorSum = xorSum ^ n;
+        }
+        return xorSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 3, 4, 5, 3, 4, 5 };
+        System.out.println("Unique element: " + findUniqueElement(nums));
+    }
+}
