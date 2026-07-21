a456b91b00562c6305338e310cd1448948007f78
Shubham Raj <sprashar889@gmail.com>
2026-07-21 16:11:44 +0530

    287-Find the Duplicate Number

---
+++ find_duplicate_287/findDuplicateNumber.java
@@ -0,0 +1,21 @@
+package find_duplicate_287;
+
+import java.util.HashSet;
+
+public class findDuplicateNumber {
+    public static int findDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return num; // found duplicate
+            }
+            seen.add(num);
+        }
+        return -1; // no duplicate found
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 3, 3, 3, 3 };
+        System.out.println("Dupicate Number is: " + findDuplicate(nums));
+    }
+}
