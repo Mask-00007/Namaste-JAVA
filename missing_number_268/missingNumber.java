acb6b0e29df2efd66d296112a1437d4c4e055b39
Shubham Raj <sprashar889@gmail.com>
2026-07-21 16:30:47 +0530

    268- Missing Number

---
+++ missing_number_268/missingNumber.java
@@ -0,0 +1,24 @@
+package missing_number_268;
+
+public class missingNumber {
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xorSum = 0;
+        // XOR WITH ALL ARRAY ELEMENTS
+        for (int num : nums) {
+            xorSum = xorSum ^ num;
+        }
+
+        // XOR WITH ALL THE ELEMENTS IN THE RANGE
+        for (int i = 0; i <= nums.length; i++) {
+            xorSum = xorSum ^ i;
+        }
+
+        return xorSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 1, 0, 3, 4 };
+        System.out.println("Missing number: " + missingNumber(nums));
+    }
+}
