90d0eaff01a8dd842cb1dcee9dd9b2d22af97dd3
Shubham Raj <sprashar889@gmail.com>
2026-07-24 06:56:49 +0530

    Selection Sort Algorithm

---
+++ sorting_algorithms/selectionSort.java
@@ -0,0 +1,30 @@
+package sorting_algorithms;
+
+// SELECTION SORT -> TIME COMPLEXITY : O(n^2)
+public class selectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 6, 3, 1, 4 };
+        selectionSort(arr);
+        System.out.println("Printing the array: ");
+        for (int value : arr) {
+            System.out.println(value + " ");
+        }
+    }
+
+}
