07822aac33e6a528cae263522505b6f072095964
Shubham Raj <sprashar889@gmail.com>
2026-07-28 08:23:41 +0530

    Upper Bound Example

---
+++ upper_bound/upperBound.java
@@ -0,0 +1,30 @@
+package upper_bound;
+
+public class upperBound {
+    static int getUpperBound(int[] arr, int target) {
+        int n = arr.length;
+        int s = 0;
+        int e = n - 1;
+        int ans = n;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            if (arr[mid] <= target) {
+                s = mid + 1;
+            } else {
+                ans = mid;
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 20, 20, 20, 20, 30, 40, 50 };
+        int target = 20;
+
+        System.out.println(getUpperBound(arr, target));
+    }
+
+}
