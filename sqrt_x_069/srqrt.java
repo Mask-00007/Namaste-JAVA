62d1f3e2d7ca09d18c1549eb244e5b436c4b4188
Shubham Raj <sprashar889@gmail.com>
2026-08-03 06:23:01 +0530

    69.Sqrt(x) More optimized approach

--- sqrt_x_069/srqrt.java
+++ sqrt_x_069/srqrt.java
@@ -1,21 +1,27 @@
 package sqrt_x_069;
 
 public class srqrt {
-
     public static int mySqrt(int x) {
-        if (x < 2)
-            return x;
-        int left = 1, right = x / 2, ans = 0;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if ((long) mid * mid <= x) {
-                ans = mid;
-                left = mid + 1;
+        int s = 1;
+        int e = x;
+        int ans = -1;
+
+        if (x == 0) {
+            return 0;
+        }
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                e = mid - 1;
             } else {
-                right = mid - 1;
+                ans = mid;
+                s = mid + 1;
             }
         }
         return ans;
+
     }
 
     public static void main(String[] args) {
