6d130437b4cc7e25654fe74079b914f3bb4e4c66
Shubham Raj <sprashar889@gmail.com>
2026-08-25 21:35:00 +0530

    Max_Element

---
+++ max_element/maxElement.java
@@ -0,0 +1,19 @@
+package max_element;
+
+public class maxElement {
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i]; // update max
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 7, 2, 9, 5 };
+        System.out.println("Maximum element: " + findMax(arr));
+    }
+
+}
