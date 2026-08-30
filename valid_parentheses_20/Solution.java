e5a3b6406c68796d98dd4109f88e3b7a73116a43
Shubham Raj <sprashar889@gmail.com>
2026-08-30 21:17:26 +0530

    20. Valid Parantheses

---
+++ valid_parentheses_20/Solution.java
@@ -0,0 +1,27 @@
+package valid_parentheses_20;
+
+import java.util.Stack;
+
+public class Solution {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Solution.isValid("()"));
+
+    }
+}
