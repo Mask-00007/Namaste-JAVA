d636ba2ee9821dbe87546afed02eb7f198a0f46c
Shubham Raj <sprashar889@gmail.com>
2026-09-06 21:27:37 +0530

    Reverse String

---
+++ reverse_string/ReverseString.java
@@ -0,0 +1,9 @@
+package reverse_string;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "hello";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(reversed);
+    }
+}
