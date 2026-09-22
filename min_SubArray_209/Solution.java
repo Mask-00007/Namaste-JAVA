77b061e8849ef6349221d2f14257e7ab17afb968
Shubham Raj <sprashar889@gmail.com>
2026-09-22 23:40:33 +0530

    209

---
+++ min_SubArray_209/Solution.java
@@ -0,0 +1,29 @@
+package min_SubArray_209;
+
+public class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int left = 0;
+        int sum = 0;
+        int minLen = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+
+            while (sum >= target) {
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+
+    // Quick test
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = { 2, 3, 1, 2, 4, 3 };
+        int target = 7;
+        System.out.println("Minimum length = " + sol.minSubArrayLen(target, nums));
+    }
+}
