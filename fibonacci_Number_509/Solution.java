a57cee77d86b44d76da3a4faffc8ae003290a831
Shubham Raj <sprashar889@gmail.com>
2026-09-24 16:40:52 +0530

    509.Fibonacci Number

---
+++ fibonacci_Number_509/Solution.java
@@ -0,0 +1,19 @@
+package fibonacci_Number_509;
+
+public class Solution {
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int ans = fib(n - 1) + fib(n - 2);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+}
