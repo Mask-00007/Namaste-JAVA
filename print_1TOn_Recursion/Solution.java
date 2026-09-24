ed92d23a35c0971277fce29620bc46b2a31edc34
Shubham Raj <sprashar889@gmail.com>
2026-09-24 17:45:05 +0530

    Printing from 1 To N using recursion

---
+++ print_1TOn_Recursion/Solution.java
@@ -0,0 +1,18 @@
+package print_1TOn_Recursion;
+
+public class Solution {
+    static void printNumbers(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        printNumbers(n - 1);
+        System.out.println(n);
+
+    }
+
+    public static void main(String[] args) {
+        printNumbers(10);
+    }
+
+}
