61028ef9a3991dcaed3c27cfd05352cff355a025
Shubham Raj <sprashar889@gmail.com>
2026-09-25 17:28:44 +0530

    Longest Substring

---
+++ longest_Substring/Solution.java
@@ -0,0 +1,31 @@
+package longest_Substring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (map.containsKey(c)) {
+                // Move left pointer to avoid duplicates
+                left = Math.max(left, map.get(c) + 1);
+            }
+
+            map.put(c, right);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.lengthOfLongestSubstring("abcabcbb"));
+
+    }
+}
